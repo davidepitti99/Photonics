50e7ab048e21d2a75db07cb1a3e8cfa1e8775601
Davide Pittilini <davidepitti99@gmail.com>
2024-07-10 00:18:37 +0200

    PBG maps

--- set_plot_params.py
+++ set_plot_params.py
@@ -1,11 +1,12 @@
 import matplotlib as mpl
 
 def set_plot_params():
-    mpl.rcParams['lines.linewidth'] = 2
+    mpl.rcParams['lines.linewidth'] = 1.5
     mpl.rcParams['lines.linestyle'] = '-'
     mpl.rcParams['axes.grid'] = True
+    mpl.rcParams['grid.alpha'] = 0.5
     mpl.rcParams['xtick.top'] = True
-    mpl.rcParams['ytick.right'] = True
+    mpl.rcParams['ytick.right'] = True 
     mpl.rcParams['xtick.minor.visible'] = True
     mpl.rcParams['ytick.minor.visible'] = True
     mpl.rcParams['xtick.direction'] = 'in'
